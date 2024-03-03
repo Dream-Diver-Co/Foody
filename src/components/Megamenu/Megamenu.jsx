091cd8363e4e186711ab12c7576706980b8c555f
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-03-03 15:24:40 +0600

    Megamenu Updated

--- src/components/Megamenu/Megamenu.jsx
+++ src/components/Megamenu/Megamenu.jsx
@@ -3,23 +3,66 @@ import './Megamenu.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPizzaSlice, faBreadSlice, faFish, faCarrot, faCookie } from '@fortawesome/free-solid-svg-icons'; // Import different icons
+import pizzaImage from '../../assets/biryani.png';
+import breadImage from '../../assets/biryani2.png';
+import fishImage from '../../assets/biryani3.png';
+import carrotImage from '../../assets/biryani2.png';
+import cookieImage from '../../assets/biryani3.png';
 
 const Megamenu = () => {
+  const menuItems = [
+    {
+      icon: faPizzaSlice,
+      name: 'Bangladesh Items',
+      description: 'Delicious Bangladeshi cuisine',
+      image: pizzaImage
+    },
+    {
+      icon: faBreadSlice,
+      name: 'Indian Items',
+      description: 'Authentic Indian flavors',
+      image: breadImage
+    },
+    {
+      icon: faFish,
+      name: 'Bangladesh Items',
+      description: 'Fresh seafood from Bangladesh',
+      image: fishImage
+    },
+    {
+      icon: faCarrot,
+      name: 'Netherland Items',
+      description: 'Healthy Dutch delicacies',
+      image: carrotImage
+    },
+    {
+      icon: faCookie,
+      name: 'African Items',
+      description: 'Traditional African treats',
+      image: cookieImage
+    },
+    {
+      icon: faPizzaSlice,
+      name: 'Japanese Items',
+      description: 'Exquisite Japanese dishes',
+      image: pizzaImage
+    }
+  ];
+
   return (
     <div className="megamenu">
       <h3>Food Menu</h3>
-      <ul>
-        <div className="row">
-          <li><FontAwesomeIcon icon={faPizzaSlice} /> <Link to='BanglaFood'>Bangladesh Items</Link></li>
-          <li><FontAwesomeIcon icon={faBreadSlice} /> <a href="#">Indian Items</a></li>
-          <li><FontAwesomeIcon icon={faFish} /> <a href="#">Bangladesh Items</a></li>
-        </div>
-        <div className="row">
-          <li><FontAwesomeIcon icon={faCarrot} /> <a href="#">Netherland Items</a></li>
-          <li><FontAwesomeIcon icon={faCookie} /> <a href="#">African Items</a></li>
-          <li><FontAwesomeIcon icon={faPizzaSlice} /> <a href="#">Japanese Items</a></li>
-        </div>
-      </ul>
+      <div className="row">
+        {menuItems.map((item, index) => (
+          <div className="menu-item" key={index}>
+            <div className="menu-item-inner">
+              <img src={item.image} alt={item.name} />
+              <h4>{item.name}</h4>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
